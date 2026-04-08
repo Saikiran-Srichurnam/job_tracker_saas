e671e9960b661fef0103671d045b1e931088b9b0
Saikiran Srichurnam <srichurnamsaikiran628@gmail.com>
2026-04-08 19:44:39 +0530

    add configurations

--- server/app.js
+++ server/app.js
@@ -3,4 +3,18 @@ import cors from "cors";
 
 const app = express();
 
+// basic configurations
+app.use(express.json({ limit: "16kb" }));
+app.use(urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+// cors configurations
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-type", "Authorization"],
+  }),
+);
 export { app };
