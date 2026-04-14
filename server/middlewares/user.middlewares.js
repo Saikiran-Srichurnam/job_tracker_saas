bead8c256fcee3b84191f988a7921f5bfe62cfc0
Saikiran Srichurnam <srichurnamsaikiran628@gmail.com>
2026-04-14 06:33:12 +0530

    add user middleware in middlewares

---
+++ server/middlewares/user.middlewares.js
@@ -0,0 +1,44 @@
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt, { decode } from "jsonwebtoken";
+
+const VerifyJWT = asyncHandler(async (req, _, next) => {
+  try {
+    //1 Get token from cookies or headers
+    const token =
+      req.cookies?.accessToken ||
+      req.headers.authorization?.replace("Bearer ", "");
+
+    if (!token) {
+      throw new ApiError(401, "Unauthorized Request");
+    }
+
+    // 2 Verify token safely
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    // 3 Find user
+    const user = await prisma.user.findUnique({
+      where: {
+        id: decodedToken.id,
+      },
+      select: {
+        id: true,
+        email: true,
+      },
+    });
+
+    if (!user) {
+      throw new ApiError(400, "Invalid accessToken");
+    }
+
+    //4 Attach user
+    req.user = user;
+
+    next();
+  } catch (error) {
+    console.log("Invalid access token:", error);
+    throw new ApiError(401, error.message);
+  }
+});
+
+export { VerifyJWT };
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,6 +3,7 @@ import { loginUser, registerUser } from "../controllers/user.controllers.js";
 
 const router = Router();
 
+// public Routes
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 
