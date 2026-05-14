1bdaa2f5414cdf76903848ab173fb6abc1bad5ca
Saikiran Srichurnam <srichurnamsaikiran628@gmail.com>
2026-05-14 15:29:13 +0530

    add styling for edit, delete btns and jobs list

--- client/src/components/Dashboard/JobsList.jsx
+++ client/src/components/Dashboard/JobsList.jsx
@@ -1,21 +1,7 @@
 import React from "react";
 
 function JobsList({ jobs }) {
-  // const [jobs, setJobs] = useState([]);
-
-  // useEffect(() => {
-  //   const fetchAllJobs = async () => {
-  //     try {
-  //       const res = await getAllJobs();
-  //       console.log(res);
-  //       setJobs(res.data.jobs);
-  //     } catch (error) {
-  //       console.log(error.message);
-  //       return;
-  //     }
-  //   };
-  //   fetchAllJobs();
-  // }, []);
+  
   return (
     <div className="bg-white/10 border border-white/10 rounded-2xl p-6 mt-8 shadow-2xl">
       <div className="flex justify-between items-center mb-5">
@@ -28,10 +14,19 @@ function JobsList({ jobs }) {
       <div className="space-y-4">
         {jobs.map((job) => (
           <div
-            className="p-4 rounded-xl bg-white/5 border border-white/10 hover:duration-300 hover:bg-white/30"
+            className="p-4 rounded-xl bg-white/5 border border-white/10 hover:duration-300 hover:border-white/60 hover:text-slate-300 w-full flex justify-between items-center"
             key={job.id}
           >
             {job.role} - {job.company}
+            <div className="mr-20 flex gap-10">
+              <button className="hover:cursor-pointer px-4 py-2 hover:bg-blue-500/20 bg-gray-500/20 text-blue-300 rounded-lg transition duration-300 border border-gray-400/30 text-sm font-semibold backdrop-blur-md hover:text-blue-500" 
+              onClick={handleEditBtn}>
+                ✏️ edit
+              </button>
+              <button className="hover:cursor-pointer px-4 py-2 hover:bg-red-500/20 bg-red-500/5 text-red-300 hover:text-white rounded-lg transition duration-300 border border-red-400/30 text-sm font-semibold backdrop-blur-md">
+                ❌
+              </button>
+            </div>
           </div>
         ))}
       </div>
