00e0198776ef2f59b601407476c18af4423b81e6
Saikiran Srichurnam <srichurnamsaikiran628@gmail.com>
2026-05-17 09:49:01 +0530

    removed unused react hooks

--- client/src/components/Dashboard/AddJobModal.jsx
+++ client/src/components/Dashboard/AddJobModal.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect} from "react";
 import { createJob, updateJob } from "../../services/jobsApi.js";
 
 function AddJobModal({
