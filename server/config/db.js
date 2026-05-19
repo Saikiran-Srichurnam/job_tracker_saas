97a41f492b9e5ec82f2d3ca7b7009e2e7ffefd27
Saikiran Srichurnam <srichurnamsaikiran628@gmail.com>
2026-05-19 22:23:56 +0530

    fix prisma db connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import pg from "pg";
 
-// 1. Setup the PG Connection Pool
-const pool = new pg.Pool({ 
-    user: 'postgres',
-    host: '127.0.0.1',
-    database: 'job_tracker_saas', // Make sure this matches your Postgres DB name!
-    password: 'kiran2002',       // Hardcode it here just to test
-    port: 5432,
-});
+const prisma = new PrismaClient();
 
-// 2. Setup the Prisma Adapter
-const adapter = new PrismaPg(pool);
-
-// 3. Export a single instance to use everywhere
-export const prisma = new PrismaClient({ adapter });
+export { prisma };
