5dac2a47514d7a0cd027fc6d7669eb87c9d68272
Saikiran Srichurnam <srichurnamsaikiran628@gmail.com>
2026-05-21 06:56:27 +0530

    fix App component capitalization

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
 } from "react-router-dom";
+import App from "./App.jsx";
 import Layout from "./Layout.jsx";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
