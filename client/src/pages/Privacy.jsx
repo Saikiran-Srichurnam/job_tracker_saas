dfdc8e103d7f698932ebc13eb53197372c5e0187
Saikiran Srichurnam <srichurnamsaikiran628@gmail.com>
2026-05-22 07:20:14 +0530

    update terms and privacy pages in darkmode

--- client/src/pages/Privacy.jsx
+++ client/src/pages/Privacy.jsx
@@ -7,43 +7,45 @@ function Privacy() {
     navigate("/dashboard");
   };
   return (
-    <div className="max-w-4xl mx-auto px-6 py-12 text-gray-800 dark:text-white">
-      <h1 className="text-4xl font-bold mb-6">Privacy Policy</h1>
+    <div className="w-full h-screen dark:bg-gray-800 flex justify-center items-center text-gray-800 dark:text-white">
+      <div className="max-w-4xl w-full mx-auto px-6 py-12 ">
+        <h1 className="text-4xl font-bold mb-6">Privacy Policy</h1>
 
-      <p className="mb-4">
-        JobTrack Pro respects your privacy and is committed to protecting your
-        personal information.
-      </p>
+        <p className="mb-4">
+          JobTrack Pro respects your privacy and is committed to protecting your
+          personal information.
+        </p>
 
-      <h2 className="text-2xl font-semibold mt-8 mb-3">
-        Information We Collect
-      </h2>
+        <h2 className="text-2xl font-semibold mt-8 mb-3">
+          Information We Collect
+        </h2>
 
-      <p className="mb-4">
-        We may collect account details, job application data, and usage
-        information to improve your experience.
-      </p>
+        <p className="mb-4">
+          We may collect account details, job application data, and usage
+          information to improve your experience.
+        </p>
 
-      <h2 className="text-2xl font-semibold mt-8 mb-3">How We Use Data</h2>
+        <h2 className="text-2xl font-semibold mt-8 mb-3">How We Use Data</h2>
 
-      <p className="mb-4">
-        Your data is used only for managing your job tracking experience and
-        improving platform features.
-      </p>
+        <p className="mb-4">
+          Your data is used only for managing your job tracking experience and
+          improving platform features.
+        </p>
 
-      <h2 className="text-2xl font-semibold mt-8 mb-3">Data Security</h2>
+        <h2 className="text-2xl font-semibold mt-8 mb-3">Data Security</h2>
 
-      <p>
-        We implement reasonable security measures to protect your personal
-        information.
-      </p>
-      <div className="space-y-3 pt-6 border-t border-white/10">
-        <button
-          onClick={() => handleCloseBtn()}
-          className="w-full bg-red-500/20 hover:bg-red-500/80 text-red-600 dark:text-white transition-all duration-300 py-3 rounded-xl font-medium hover:scale-[1.02]"
-        >
-          Go to Dashboard
-        </button>
+        <p>
+          We implement reasonable security measures to protect your personal
+          information.
+        </p>
+        <div className="space-y-3 pt-6 border-t border-white/10">
+          <button
+            onClick={() => handleCloseBtn()}
+            className="w-full bg-red-500/20 hover:bg-red-500/80 text-red-600 dark:text-white transition-all duration-300 py-3 rounded-xl font-medium hover:scale-[1.02]"
+          >
+            Go to Dashboard
+          </button>
+        </div>
       </div>
     </div>
   );
--- client/src/pages/Terms.jsx
+++ client/src/pages/Terms.jsx
@@ -7,41 +7,43 @@ function Terms() {
     navigate("/dashboard");
   };
   return (
-    <div className="max-w-4xl mx-auto px-6 py-12 text-gray-800 dark:text-white">
-      <h1 className="text-4xl font-bold mb-6">Terms & Conditions</h1>
+    <div className="w-full h-screen dark:bg-gray-800 flex justify-center items-center text-gray-800 dark:text-white">
+      <div className="max-w-4xl mx-auto px-6 py-12">
+        <h1 className="text-4xl font-bold mb-6">Terms & Conditions</h1>
 
-      <p className="mb-4">
-        By using JobTrack Pro, you agree to follow these terms and conditions.
-      </p>
+        <p className="mb-4">
+          By using JobTrack Pro, you agree to follow these terms and conditions.
+        </p>
 
-      <h2 className="text-2xl font-semibold mt-8 mb-3">
-        User Responsibilities
-      </h2>
+        <h2 className="text-2xl font-semibold mt-8 mb-3">
+          User Responsibilities
+        </h2>
 
-      <p className="mb-4">
-        Users are responsible for maintaining accurate information and securing
-        their account credentials.
-      </p>
+        <p className="mb-4">
+          Users are responsible for maintaining accurate information and
+          securing their account credentials.
+        </p>
 
-      <h2 className="text-2xl font-semibold mt-8 mb-3">Acceptable Usage</h2>
+        <h2 className="text-2xl font-semibold mt-8 mb-3">Acceptable Usage</h2>
 
-      <p className="mb-4">
-        You agree not to misuse the platform or attempt unauthorized access.
-      </p>
+        <p className="mb-4">
+          You agree not to misuse the platform or attempt unauthorized access.
+        </p>
 
-      <h2 className="text-2xl font-semibold mt-8 mb-3">Changes to Terms</h2>
+        <h2 className="text-2xl font-semibold mt-8 mb-3">Changes to Terms</h2>
 
-      <p>
-        We may update these terms periodically. Continued use of the platform
-        indicates acceptance of updated terms.
-      </p>
-      <div className="space-y-3 pt-6 border-t border-white/10">
-        <button
-          onClick={() => handleCloseBtn()}
-          className="w-full bg-red-500/20 hover:bg-red-500/80 text-red-600 dark:text-white transition-all duration-300 py-3 rounded-xl font-medium hover:scale-[1.02]"
-        >
-          Go to Dashboard
-        </button>
+        <p>
+          We may update these terms periodically. Continued use of the platform
+          indicates acceptance of updated terms.
+        </p>
+        <div className="space-y-3 pt-6 border-t border-white/10">
+          <button
+            onClick={() => handleCloseBtn()}
+            className="w-full bg-red-500/20 hover:bg-red-500/80 text-white transition-all duration-300 py-3 rounded-xl font-medium hover:scale-[1.02]"
+          >
+            Go to Dashboard
+          </button>
+        </div>
       </div>
     </div>
   );
